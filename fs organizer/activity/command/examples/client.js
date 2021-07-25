8cce73f5123132ac6a8444afaa557b2ac04ff099
abhishektomar248 <87928597+abhishektomar248@users.noreply.github.com>
2021-07-25 16:10:32 +0530

    Delete fs organizer/activity/command directory

--- fs organizer/activity/command/examples/client.js
+++
@@ -1,7 +0,0 @@
-// import 
-// require -> path of the the file from 
-// where you want to import fxns or variables
-let libExportObj = require("./lib");
-console.log("I am client file ");
-console.log(libExportObj.varName);
-console.log(libExportObj.fxn());
--- fs organizer/activity/command/tree.js
+++
@@ -1,8 +0,0 @@
-function treeFn(src) {
-    console.log("tree command executed with path : " + src);
-    
-}
-
-module.exports = {
-    treefxn: treeFn
-}
